b4ca5bc5a37b0787ae7d48e472e47ec7ac1b1ee5
Locke Monstah <Mloughran87@gmail.com>
2018-10-13 21:33:19 -0400

    csswork

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Setting up the variables, the four crystals get thier own var, and total score
+
+var randomnumber = 0;
+
+var crystal1 = randomnumber(1,12);
+var crystal2 = randomnumber(1,12);
+var crystal3 = randomnumber(1,12);
+var crystal4 = randomnumber(1,12);
+
+var totalScore = 0;
+var scoreTarget = 0;
+
