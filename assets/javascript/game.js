f26c309448becf6cacee4b2afac56cad0d3e2ecd
Locke Monstah <Mloughran87@gmail.com>
2018-10-13 22:28:22 -0400

    JS added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 
 var randomnumber = 0;
 
+var crystals = Math.floor(Math.random() * 11) + 1;
 var crystal1 = randomnumber(1,12);
 var crystal2 = randomnumber(1,12);
 var crystal3 = randomnumber(1,12);
@@ -10,3 +11,10 @@ var crystal4 = randomnumber(1,12);
 var totalScore = 0;
 var scoreTarget = 0;
 
+function targetNumber(){
+    var scoreTarget = Math.floor(Math.random() * 120) + 1;
+
+    if (totalScore === scoreTarget){
+        $("wins").html()
+    }
+}
